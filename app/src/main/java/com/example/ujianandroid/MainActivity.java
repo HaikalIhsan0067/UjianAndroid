693f5cd5a0f6d72e497ee158c9a06a4f6a492b81
HaikalIhsan0067 <haikalihsan676@gmail.com>
2023-05-10 19:59:21 +0700

    TAMBAH UMUR BARU

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -32,15 +32,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
-                String isian_umur = edUmur.getText().toString();
+                Integer isian_umur = Integer.valueOf(edUmur.getText().toString());
 
                 if(isian_nama_depan.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
                 }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang).concat(" ").concat(isian_umur);
+                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
                     daftar_nama.clear();
-                    daftar_nama.add(nama_lengkap);
-                    edUmur.setText("");
+                    for (int i = 0; i < isian_umur; i++) {
+                        daftar_nama.add(nama_lengkap);
+                    }
+                    //daftar_nama.add(nama_lengkap);
+                    //edUmur.setText("");
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
